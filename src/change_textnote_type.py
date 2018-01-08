ad7d5237a5d710c9b1f4a307d31f16ab08d0947d
Spencer <spencer.michael.q@gmail.com>
2018-01-08 22:31:30 +0800

    already working code (used on G1 architectural sheets) (cleaned)

--- src/change_textnote_type.py
+++ src/change_textnote_type.py
@@ -11,7 +11,6 @@ provided that the TextNotes are inside any of the following View Classes:
     ViewDrafting
     ViewPlan
     ViewSection
-
     Args:
         _run:           (bool) True to change the Note Types,
                         False to simply view the items
@@ -19,11 +18,9 @@ provided that the TextNotes are inside any of the following View Classes:
                         False to change for entire doc
         _type1:         (TextNoteType) first basis, i.e. other note types
                         will be changed to this
-
     Returns:
         OUT:            if _run = True, all unchanged TextNotes with their Id
                         if _run = False, all TextNotes inside the doc
-
 """
 
 import clr
@@ -33,18 +30,13 @@ clr.AddReference('RevitServices')
 clr.AddReference('RevitNodes')
 
 from Autodesk.Revit.DB import *
-from Autodesk.Revit.Exceptions import ArgumentException
 from RevitServices.Persistence import DocumentManager
 import Revit
 
-from System.Collections.Generic import List
-# https://msdn.microsoft.com/en-us/library/bb896387%28v=vs.110%29.aspx?f=255&MSPPError=-2147217396
-from System import Type
-
 clr.ImportExtensions(Revit.Elements)
 
-doc                 = DocumentManager.Instance.CurrentDBDocument
-uidoc               = DocumentManager.Instance.CurrentUIDocument
+doc = DocumentManager.Instance.CurrentDBDocument
+uidoc = DocumentManager.Instance.CurrentUIDocument
 
 # True = change types, False = view
 toggle              = bool(IN[0])
@@ -73,8 +65,33 @@ type_2_3            = UnwrapElement(IN[7])
 # 3.5mm ISO (from 4.0)
 type_3_5            = UnwrapElement(IN[8])
 
+# -------------- Bold Types ------------- #
+
+type_0_25_b         = UnwrapElement(IN[9])
+type_0_5_b          = UnwrapElement(IN[10])
+type_1_0_b          = UnwrapElement(IN[11])
+type_1_5_b          = UnwrapElement(IN[12])
+type_2_b            = UnwrapElement(IN[13])
+type_2_3_b          = UnwrapElement(IN[14])
+type_3_5_b          = UnwrapElement(IN[15])
+
+
+# -------------- Bold Types ------------- #
+
 
 # -------------- Utilities ------------- #
+
+
+# NOTE: ActiveView only works if you actually go inside the viewport
+# i.e., not just the sheet
+def view_toggle():
+    """if True: run only for active view, False: entire doc"""
+
+    # FilteredElementCollector also takes in ICollection of ElementIds
+    return FilteredElementCollector(doc, doc.ActiveView.Id)     \
+        if active_view_only is True                             \
+        else FilteredElementCollector(doc)
+
 
 class MyFailureHandler(IFailuresPreprocessor):
     def processFailures(failures_accessor):
@@ -82,140 +99,130 @@ class MyFailureHandler(IFailuresPreprocessor):
 
         return FailureProcessingResult.Continue
 
-num_of_elements_before = FilteredElementCollector(doc).             \
-                            OfClass(clr.GetClrType(TextElement)).   \
-                            ToElementIds().                         \
-                            Count
 
 # -------------- Utilities ------------- #
 
 
-view_multi_class_list = List[Type]()
-view_multi_class_list.Add(clr.GetClrType(ViewDrafting))
-view_multi_class_list.Add(clr.GetClrType(ViewPlan))
-view_multi_class_list.Add(clr.GetClrType(ViewSection))
+# -------------- Road map  ------------- #
 
-view_multi_class_filter = ElementMulticlassFilter(view_multi_class_list)
+"""
+2018_01_008 - CHANGE TEXT EVEN IF YOU ARE AT VIEWHSEET LEVEL
+I.E. GET ALL VIEWPORTS AND ALL ELEMENTS INSIDE VIEWPORTS
 
-view_id_collector = FilteredElementCollector(doc).                  \
-                    WherePasses(view_multi_class_filter).           \
-                    ToElementIds()
+fOR vIEW ELEMENTS MODE - SHOW ALL THE FONTS
 
-text_note_collector = []
+SHOW NOTIFICATIONS FOR CHANGED TEXT
+	> FROM WHAT TO WHAT (SIZE, FONT)
+	> NUMBER OF CHANGED TEXT, UNCHANGED TEXT
+"""
 
-# TODO: apparently, this loop actually makes revit go inside the views
-for view_id in view_id_collector:
+# TODO: (long-term): make the inputs more dynamo, (probably using exec)
 
-    '''ArgumentException:
-    viewId is not a view. -or- viewId is not valid for element iteration, 
-    because it has no way of representing drawn elements. 
-    Many view templates will fail this check.
-    
-    Probably because the Dynamo Script was catching Sections that 
-    DON'T even exist, so I place it in a try-except instead
-    '''
+# -------------- Road map  ------------- #
 
-    # NOTE: ActiveView only works if you actually go inside the viewport
-    # i.e., not just the sheet
-    try:
-        '''Toggle active view only
-        and entire doc
-        '''
-        if active_view_only:
-            inner_note_collector = FilteredElementCollector(doc, doc.ActiveView.Id) \
-                                    .OfCategory(BuiltInCategory.OST_TextNotes)      \
-                                    .ToElements()
 
-            for inner_note in inner_note_collector:
-                text_note_collector.append(inner_note)
-
-            # stop outer iteration, i.e. you only need elements from ActiveView
-            break
-
-        else:
-            inner_note_collector = FilteredElementCollector(doc, view_id)           \
-                .OfCategory(BuiltInCategory.OST_TextNotes)                          \
-                .ToElements()
-
-            for inner_note in inner_note_collector:
-                text_note_collector.append(inner_note)
-
-    except ArgumentException:
-        continue
+text_note_collector = view_toggle().\
+                        OfCategory(BuiltInCategory.OST_TextNotes)
 
 
 if toggle is True:
 
     unchanged_text = []
 
-    with Transaction(doc, 'Change Note Types DYNAMOREVAPI') as t:
-
-        t.Start()
-        # fail_opt = t.GetFailureHandlingOptions()
-        # fail_opt.SetFailuresPreprocessor(MyFailureHandler())
-        # t.SetFailureHandlingOptions(fail_opt)
-
-        for text in text_note_collector:
-
-            text_note_type = text.TextNoteType
-
-            text_size = text_note_type.get_Parameter(BuiltInParameter.TEXT_SIZE).AsDouble()
-            text_size = UnitUtils.ConvertFromInternalUnits(text_size,
-                                                           DisplayUnitType.DUT_MILLIMETERS)
-
-            type_list = IN[2:]
-
-            if (text_note_type in type_list) and (0 <= text_size <= 4.3):
-                continue
-
-            if 0.1 <= text_size <= 0.45:
-                # change size to 0.25
-                text.TextNoteType = type_0_25
-                # unchanged_text.append([text, 'Change to 0.25 from {}'.format(text_size)])
-
-            elif 0.45 < text_size <= 0.9:
-                text.TextNoteType = type_0_5
-
-            elif 0.9 < text_size <= 1.4:
-                text.TextNoteType = type_1_0
-
-            elif 1.4 < text_size <= 1.9:
-                text.TextNoteType = type_1_5
-
-            elif 1.9 < text_size <= 2.2:
-                text.TextNoteType = type_2
-
-            elif 2.2 < text_size <= 3.4:
-                text.TextNoteType = type_2_3
-
-            elif 3.4 < text_size <= 4.3:
-                text.TextNoteType = type_3_5
-
-            else:
-                # append the UNchanged textNotes for viewing in Dynamo
-                unchanged_text.append([text, 'Unchanged: probably too large, size={}'.format(text_size)])
-                # symbols.append(text.Symbol)
-
-        num_of_elements_after = FilteredElementCollector(doc).          \
+    num_of_elements_before = view_toggle().                             \
                                 OfClass(clr.GetClrType(TextElement)).   \
                                 ToElementIds().                         \
                                 Count
 
-        # assert that num of TextNotes is the same before and after for entire doc
+    with Transaction(doc, 'Change Note Types DYNAMOREVAPI') as t:
+
+        t.Start()
+
+        for text in text_note_collector:
+
+            text_note_type = text.TextNoteType
+            text_size_param = BuiltInParameter.TEXT_SIZE
+
+            text_size = text_note_type.get_Parameter(text_size_param).AsDouble()
+            text_size = UnitUtils.ConvertFromInternalUnits(text_size,
+                                                           DisplayUnitType.DUT_MILLIMETERS)
+            text_bold_param_original = text_note_type \
+                                        .get_Parameter(BuiltInParameter.TEXT_STYLE_BOLD) \
+                                        .AsInteger()
+            type_list = IN[2:]
+            text_note_font = text_note_type.get_Parameter(BuiltInParameter.TEXT_FONT).AsString()
+
+            # cancel if the TextNoteType is already ISOCPEUR AND if size within 0.1 - 4.3
+            if ('ISOCPEUR' in text_note_font) and (0 <= text_size <= 4.3):
+                unchanged_text.append('Text is already ISOCPEUR')
+                continue
+
+            if 0.1 <= text_size <= 0.45:
+                # change size to 0.25
+                if not text_bold_param_original:
+                    text.TextNoteType = type_0_25
+                elif text_bold_param_original:
+                    text.TextNoteType = type_0_25_b
+
+            elif 0.45 < text_size <= 0.9:
+                if not text_bold_param_original:
+                    text.TextNoteType = type_0_5
+                elif text_bold_param_original:
+                    text.TextNoteType = type_0_5_b
+
+
+            elif 0.9 < text_size <= 1.4:
+                if not text_bold_param_original:
+                    text.TextNoteType = type_1_0
+                elif text_bold_param_original:
+                    text.TextNoteType = type_1_5_b
+
+            elif 1.4 < text_size <= 1.9:
+                if not text_bold_param_original:
+                    text.TextNoteType = type_1_5
+                elif text_bold_param_original:
+                    text.TextNoteType = type_1_5_b
+
+            elif 1.9 < text_size <= 2.2:
+                if not text_bold_param_original:
+                    text.TextNoteType = type_2
+                elif text_bold_param_original:
+                    text.TextNoteType = type_2_b
+
+            elif 2.2 < text_size <= 3.4:
+                if not text_bold_param_original:
+                    text.TextNoteType = type_2_3
+                elif text_bold_param_original:
+                    text.TextNoteType = type_2_3_b
+
+
+            elif 3.4 < text_size <= 4.3:
+                if not text_bold_param_original:
+                    text.TextNoteType = type_3_5
+                elif text_bold_param_original:
+                    text.TextNoteType = type_3_5_b
+
+            else:
+                unchanged_text.append([text, 'Unchanged: probably too large, size={}'.format(text_size)])
+                # symbols.append(text.Symbol)
+
+        num_of_elements_after = view_toggle().                          \
+                                OfClass(clr.GetClrType(TextElement)).   \
+                                ToElementIds().                         \
+                                Count
+
+        # assert that num of TextNotes is the same before and after
         if num_of_elements_before != num_of_elements_after:
             t.RollBack()
             OUT = 'Error Occurred, Some items were deleted'
+            uidoc.RefreshActiveView()
 
         else:
             t.Commit()
             OUT = unchanged_text
+            uidoc.RefreshActiveView()
 
-        uidoc.RefreshActiveView()
-
-
-# DYNAMO CALLS THIS ANOTHER TRANSACTION?? WTH?
-# # just display the TextNotes if toggle = False
-# elif toggle is False:
-#     OUT = [text.ToDSType(False) for text in text_note_collector]
-#     # OUT = text_note_collector.ToElementIds().Count
-#     uidoc.RefreshActiveView()
+# Note to self: ToDSType may be causing accidental deletion of the Texts
+# because maybe they're 'OWNBED by Dynamo????' (not really sure)
+elif toggle is False:
+    OUT = text_note_collector
