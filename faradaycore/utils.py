62e2d6df26c1c60a83002ee5781ea9ebb7d3302e
Spencer <spencer.michael.q@gmail.com>
2018-01-09 19:53:24 +0800

    utilities to add

---
+++ faradaycore/utils.py
@@ -0,0 +1,14 @@
+"""
+# TODO:
+Logging utils
+timer for how long a function takes
+subprocess.Popen
+"""
+
+import logging
+import sys
+import subprocess
+
+from uuid import uuid4
+from functools import wraps
+from time import time
