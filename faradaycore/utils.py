069d3b12cd561d880be735b82b2469734774cfa5
Spencer <spencer.michael.q@gmail.com>
2018-01-09 20:00:22 +0800

    add header

--- faradaycore/utils.py
+++ faradaycore/utils.py
@@ -1,11 +1,21 @@
+# Faraday: A Dynamo Plugin (GPL) started by Michael Spencer Quinto
+# This file is part of Faraday.
+#
+# You should have received a copy of the GNU General Public License
+# along with Faraday; If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
 """
 # TODO:
 Logging utils
 timer for how long a function takes
 subprocess.Popen
+os.path.join
 """
 
 import logging
+import os
 import sys
 import subprocess
 
