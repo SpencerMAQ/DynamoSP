3c97c9f5d7c7382d37d6d690495209871007b869
Spencer <spencer.michael.q@gmail.com>
2018-01-21 17:56:22 +0800

    added copy_files.py

---
+++ __git_utilities/copy_files.py
@@ -0,0 +1,20 @@
+# Faraday: A Dynamo Plugin (GPL)
+# started by Michael Spencer Quinto <https://github.com/SpencerMAQ>
+#
+# This file is part of Faraday.
+#
+# You should have received a copy of the GNU General Public License
+# along with Faraday; If not, see <http://www.gnu.org/licenses/>.
+#
+# @license GPL-3.0+ <http://spdx.org/licenses/GPL-3.0+>
+
+"""Patterned after Mostapha's copy_files.py
+https://github.com/ladybug-tools/ladybug-dynamo/
+blob/master/plugin/copy_files.py"""
+
+import os
+import shutil
+
+
+def copy_files(src, dst, dyf=True, nodesrc=True, hbsrc=True):
+    pass
---
+++ src/README.md
@@ -0,0 +1,1 @@
+Source code for the dyf files based on the contents of their Python component
